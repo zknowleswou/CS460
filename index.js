314e8a640695456bdbd44121aba1ecba56f0080c
Zach K <zknowles@outlook.com>
2018-09-28 15:54:23 +0100

    Got a basic version of my profile page up and running. Added in a fun little rotating profile picture with description.

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ console.log("test");
 //Global variables are bad
 var profileCards = [
     {
-        fileName: "Zach1-min.jpg",
+        fileName: "./images/Zach1-min.jpg",
         description: "This day I was in Vianna Do Castelo enjoying some sun with my German friend."
+    },
+    {
+        fileName: "./images/Zach2.jpg",
+        description: "This day I got married in San Francisco"
     }
 ];
 
 $(document).ready(function () {
     var profileCard = profileCards[getRandomInt(0, profileCards.length - 1)]
-    
+    $("#profileLocationDescription").text(profileCard.description);
+    $("#profileImage").attr("src", profileCard.fileName);
 });
 
 //From Mozilla
