b08a999c7a5985814bcceb171ae0e879f755ef96
Zach K <zknowles@outlook.com>
2018-09-30 11:09:48 +0100

    Figured out a better way to make my nav reusable so that it actually works on chrome.

---
+++ homework1/AssignmentCode/nav.js
@@ -0,0 +1,27 @@
+var nav = `
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="./index.html">Home</a>
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="./history.html">History</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./learn.html">Learn</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./gettingstarted.html">Getting Started</a>
+            </li>
+        </ul>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        </div>
+    </div>
+</nav>
+`;
