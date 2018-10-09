29f3cac6b63acfe56ff289ca288d5877c636813a
Zachery Knowles <zknowles@outlook.com>
2018-10-09 08:13:15 -0700

    Filled in the node class.

--- homework3/AssignmentCode/Node.cs
+++ homework3/AssignmentCode/Node.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework3
+﻿namespace Homework3
 {
-    class Node
+    public class Node<T>
     {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data, Node<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+
     }
 }
