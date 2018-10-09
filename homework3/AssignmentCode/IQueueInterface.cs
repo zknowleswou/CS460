21576678e1ac6450e99d28c99db30a37d5a4284b
Zachery Knowles <zknowles@outlook.com>
2018-10-09 08:27:05 -0700

    Finished the queue interface

---
+++ homework3/AssignmentCode/IQueueInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public interface IQueueInterface<T>
+    {
+        /// <summary>
+        /// Add an element to the rear of the queue
+        /// </summary>
+        /// <param name="element">Element to add to the queue</param>
+        /// <returns>the element that was enqueued</returns>
+        T Push(T element);
+
+        /// <summary>
+        /// Remove and return the front element.
+        /// <returns>Returns next element and removes it from the queue</returns>
+        T Pop();
+
+        /// <summary>
+        /// Test if the queue is empty
+        /// </summary>
+        /// <returns>true if the queue is empty; otherwise false</returns>
+        bool IsEmpty();
+    }
+}
