6295b849bc14f3756e0bd8abca9f35039c4a83de
Zachery Knowles <zknowles@outlook.com>
2018-10-25 15:29:43 -0700

    Completed the color mixer

---
+++ homework4/AssignmentCode/Homework4/Controllers/ColorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework4.Models;
+using WebGrease.Css;
+
+namespace Homework4.Controllers
+{
+    [RoutePrefix("color")]
+    public class ColorController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            var model = new ColorMixModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("")]
+        // GET: Color/Details/5
+        public ActionResult Post(ColorMixModel model)
+        {
+            if(string.IsNullOrWhiteSpace(model.PrimaryColor) || string.IsNullOrWhiteSpace(model.SecondaryColor))
+                throw new ArgumentException("Missing either primary or secondary color.");
+
+            var primaryColor = ColorTranslator.FromHtml(model.PrimaryColor);
+            var secondaryColor = ColorTranslator.FromHtml(model.SecondaryColor);
+
+            var r = (int)Math.Round((secondaryColor.R + primaryColor.R) / (2f),0);
+            var g = (int)Math.Round((secondaryColor.G + primaryColor.G) / (2f), 0);
+            var b = (int)Math.Round((secondaryColor.B + primaryColor.B) / (2f), 0);
+
+            var mixedColor = Color.FromArgb(1, r, g, b);
+
+            model.MixedColor = ColorTranslator.ToHtml(mixedColor);
+
+            return View("Index", model);
+        }
+     }
+}
